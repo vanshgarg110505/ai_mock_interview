2c1f45550ac620baf0fc7b42503624e81b2aaadd
Vansh <vanshgarg2k5@gmail.com>
2025-04-26 12:58:30 +0530

    Project initiated and setup completed with shadcn UI

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+const App = () => {
+  return (
+    <div>
+      <Button>Click Me</Button>
+      <h1 className='text-4xl'>Hello</h1>
+    </div>
+  )
+}
+
+export default App
